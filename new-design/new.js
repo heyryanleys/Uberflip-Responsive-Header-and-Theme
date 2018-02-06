54d3a3ed28864ffd05ef675915a8f77ea81ef920
Ryan Leys <heyryanleys@gmail.com>
2018-02-06 14:26:29 -0500

    Starting new design

---
+++ new-design/new.js
@@ -0,0 +1,27 @@
+/* Adds Header Navigation on Homepage Only */
+function imageMapping() {
+  var thisURL = window.location.href
+  if (window.location.href.indexOf("igus.uberflip.com/?") > -1 || thisURL == "igus.uberflip.com") {
+    document.getElementById("large-top-nav").style.display = 'block';
+    document.getElementById("medium-top-nav").style.display = 'block';
+
+  } else {
+    document.getElementById("large-top-nav").style.display = 'block';
+    document.getElementById("medium-top-nav").style.display = 'block';
+  }
+}
+
+/* Run Fuctions On Page Load */
+Hubs.onLoad = function() {
+  imageMapping();
+}
+
+/* Runs Functions When Page Changes*/
+Hubs.onPageChange = function() {
+  imageMapping();
+}
+
+/* Runs Functions When Items Load */
+Hubs.onItemsLoaded = function() {
+  imageMapping();
+}
