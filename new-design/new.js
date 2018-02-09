4ff7deaa302d29aeb66de6c68e3efcacd2e16b9c
Ryan Leys <heyryanleys@gmail.com>
2018-02-09 09:37:38 -0500

    Respsonsive hero video

--- new-design/new.js
+++ new-design/new.js
@@ -1,5 +1,5 @@
 /* Adds Header Navigation on Homepage Only */
-function imageMapping() {
+/* function imageMapping() {
   var thisURL = window.location.href
   if (window.location.href.indexOf("igus.uberflip.com/?") > -1 || thisURL == "igus.uberflip.com") {
     document.getElementById("large-top-nav").style.display = 'block';
@@ -11,23 +11,4 @@ function imageMapping() {
   }
 }
 
-/* Run Fuctions On Page Load */
-Hubs.onLoad = function() {
-  imageMapping();
-}
-
-/* Runs Functions When Page Changes*/
-Hubs.onPageChange = function() {
-  imageMapping();
-}
-
-/* Runs Functions When Items Load */
-Hubs.onItemsLoaded = function() {
-  imageMapping();
-}
-
-$(document).ready(function() {
-  $(".large-overlay").css({
-    'width': ($(".page-aligner").width() + 'px')
-  });
-});
+*/
